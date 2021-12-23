b0f7a7702ace15e7b729705b4233e4458956f9bf
teetawat <teetawat.tch@gmail.com>
2021-12-23 23:27:57 +0700

    add button to first & link template website

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # frontendTemplate
 It is the template html/css. I practise frontend skill with write html css bootstrap.
+
+# template link
+
+https://tee-template-frontend.herokuapp.com/
--- index.php
+++ index.php
@@ -1,3 +1,5 @@
 <?php
-include_once("template1/index.html")
+echo "<a href='./template1/index.html'><button style='border:none; padding:1rem;border-radius:25px;'>go to template website</button></a>";
+//include_once("template1/index.html")
+// https://tee-template-frontend.herokuapp.com/template1/index.html
 ?>
