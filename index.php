72d00c12e33b901301c53b5b6da4d306d6ab1eac
teetawat <teetawat.tch@gmail.com>
2021-12-27 13:56:35 +0700

    edit layout button

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-echo "<a href='./template1/index.html'><button style='border:none; padding:1rem;border-radius:25px;display:flex;justify-content:center;align-items: center;'>go to template website</button></a>";
+echo "<a href='./template1/index.html'><button style='border:none; padding:1rem;border-radius:25px;display:flex;justify-content:center;align-items: center;'>Click, go to template website</button></a>";
 //include_once("template1/index.html")
 // https://tee-template-frontend.herokuapp.com/template1/index.html
 ?>
